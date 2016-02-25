51b7f00749fe72dc9e45a8eedeaa2cc550b85699
Jeniffer <jramirez894@misena.edu.co>
2016-02-25 18:22:05 -0500

    Initial commit

---
+++ app/src/main/java/unics/com/restaurante/MainActivity.java
@@ -0,0 +1,99 @@
+
+package unics.com.restaurante;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity
+{
+    Timer timer;
+    MyTimerTask myTimerTask;
+
+    View viewBienvenida;
+
+    boolean tiempoFinalizado = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        viewBienvenida = (View)findViewById(R.id.layoutInformacion);
+
+        timer = new Timer();
+        myTimerTask = new MyTimerTask();
+        timer.schedule(myTimerTask, 0, 3500);
+    }
+
+    class MyTimerTask extends TimerTask
+    {
+        @Override
+        public void run()
+        {
+            runOnUiThread(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    if (tiempoFinalizado)
+                    {
+                        timer.cancel();
+
+                        Intent intent = new Intent(MainActivity.this, Login.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else
+                    {
+                        Animation transparencia;
+                        transparencia = AnimationUtils.loadAnimation(MainActivity.this, R.anim.transparencia);
+                        transparencia.reset();
+                        viewBienvenida.setAnimation(transparencia);
+                        tiempoFinalizado = true;
+                    }
+
+                }
+            });
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
